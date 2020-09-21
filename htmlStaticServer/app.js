1952649b97a27da8b87e197458ea9c992687484a
wangwenlin <wangwenlin@innotechx.com>
2020-09-21 17:54:05 +0800

    修改商品

--- htmlStaticServer/app.js
+++ htmlStaticServer/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static("/wwl-daWang/dist/"));
+app.use(express.static("/"));
 app.get("/", function(req, res) {
 	res.send("Hello jinDong!");
 });
